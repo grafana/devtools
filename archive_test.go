875152ec25040a33b27b8d888272df31ac6d7166
bergquist <carl.bergquist@gmail.com>
2018-04-16 18:36:39 +0200

    refactor tests

--- archive_test.go
+++ archive_test.go
@@ -38,6 +38,24 @@ func TestGenerateUrlsForTwoAndAHalfDay(t *testing.T) {
 	assert.Equal(t, len(result), 36, "failure")
 }
 
+func TestGenerateUrlsWhenArchivedFilesExists(t *testing.T) {
+	var archivedFiles []*ArchiveFile
+
+	for i := 0; i < 12; i++ {
+		archivedFiles = append(archivedFiles, &ArchiveFile{
+			Year:  2018,
+			Month: 1,
+			Day:   1,
+			Hour:  i})
+	}
+
+	stopDate := fakeStopDate(1, 0)
+
+	result := buildUrlsDownload(archivedFiles, startDate, stopDate)
+
+	assert.Equal(t, len(result), 0, "failure")
+}
+
 func fakeStopDate(days, hours int) time.Time {
 	return time.Date(2018, time.Month(1), days, hours, 0, 0, 0, time.Local)
 }
