1176f8b8dd62c307a0fa18d8534933dc8b8bbe62
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2019-03-09 02:13:34 +0100

    server mock of github archive server serving files from filesystem

---
+++ cmd/fake-github-archive-server/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"github.com/NYTimes/gziphandler"
+)
+
+func main() {
+	var (
+		srcPath string
+	)
+
+	flag.StringVar(&srcPath, "path", "", "local path on disk where github archive events are located")
+	flag.Parse()
+
+	withoutGz := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fileName := strings.TrimSuffix(r.URL.Path, ".gz")
+		fullPath := path.Join(srcPath, fileName)
+		fmt.Println("serving file", "path", fullPath)
+
+		r.URL, _ = url.Parse(fileName)
+		w.Header().Add("Content-Type", "application/gzip")
+
+		_, err := os.Stat(fullPath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				w.WriteHeader(404)
+				return
+			}
+			log.Fatal(err)
+		}
+
+		var buf bytes.Buffer
+		gzipWriter := gzip.NewWriter(&buf)
+
+		f, err := os.Open(fullPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = io.Copy(gzipWriter, f)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := gzipWriter.Close(); err != nil {
+			log.Fatal(err)
+		}
+
+		reader := bytes.NewReader(buf.Bytes())
+		http.ServeContent(w, r, fullPath, time.Now(), reader)
+	})
+
+	withGz := gziphandler.GzipHandler(withoutGz)
+
+	http.Handle("/", withGz)
+	http.ListenAndServe("0.0.0.0:8000", nil)
+}
